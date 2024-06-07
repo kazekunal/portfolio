ec8db06c8aa2dc84a24e8664f79a229f8506cace
kunal passan <Kunalpassan30@gmail.com>
2024-06-07 15:47:49 +0530

    EmailSection updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
-// import EmailSection from "./components/EmailSection";
+import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 
@@ -15,7 +15,7 @@ export default function Home() {
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
-        {/* <EmailSection /> */}
+        <EmailSection />
       </div>
       <Footer />
     </main>
