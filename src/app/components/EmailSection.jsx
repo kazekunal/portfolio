096a324bcdd8fa45195ff12e22d93ee0dcb5507f
kunal passan <Kunalpassan30@gmail.com>
2024-06-07 15:51:45 +0530

    py changed

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -43,7 +43,7 @@ const EmailSection = () => {
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      className="grid md:grid-cols-2 my-12 md:my-12 py-8 gap-4 relative"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
@@ -56,7 +56,7 @@ const EmailSection = () => {
           open. Whether you have a question or just want to say hi, I&apos;ll
             get back to you!
         </p>
-        <div className="socials flex flex-row gap-8">
+        <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/kazekunal">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
