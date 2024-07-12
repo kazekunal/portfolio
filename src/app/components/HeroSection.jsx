8d27865ffbd81ad24eeda537288b10cea8798983
kunal passan <Kunalpassan30@gmail.com>
2024-07-12 15:06:25 +0530

    resume updated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
               My Projects
             </Link>
             <Link
-              href="Resume_current.pdf"  target="_blank"
+              href="Resume.pdf"  target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
