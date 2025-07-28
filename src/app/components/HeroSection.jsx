51f39ceac3f1d694abc47b3e667c23e82a011ace
kunal passan <Kunalpassan30@gmail.com>
2025-07-28 23:54:25 +0530

    resume updated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,11 +49,11 @@ const HeroSection = () => {
               My Projects
             </Link>
             <Link
-              href="resume_ats.pdf"  target="_blank"
+              href="resume_4thyr.pdf"  target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                View Resume
               </span>
             </Link>
           </div>
